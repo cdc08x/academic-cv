6fe04b23631f5d0e8ca941b6c08351abdf066a9e
Claudio Di Ciccio <dc.claudio@gmail.com>
2026-01-09 12:56:45 +0100

    Revised supervision section, with two new formatting commands (CvAcr and CvAcrDesc)

--- create_first_page_thumbnail.sh
+++ create_first_page_thumbnail.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # Author: Claudio Di Ciccio
-/home/cdc08x/Code/CV/CVconvert -quality 100 -density 600 -resample 300 -background white -alpha remove -resize 20% ClaudioDiCiccio-CV.pdf[0] ClaudioDiCiccio-CV.jpeg
+convert -quality 100 -density 600 -resample 300 -background white -alpha remove -resize 20% ClaudioDiCiccio-CV.pdf[0] ClaudioDiCiccio-CV.jpeg
